f5189e79ab926ece6704e841f89db5229592b20a
traore023 <traorem023@gmail.com>
2023-12-02 23:49:21 +0300

    fixed

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import MeetupList from "../components/meetups/MeetupList";
 import Head from "next/head";
+import {MongoClient} from "mongodb";
 
 const HomePage = (props) => {
     return (
@@ -26,12 +27,24 @@ export async function getStaticProps() {
     // do DB connection here, data fetch, etc. Never reaches client machine
     // will be executed on the server, because it is run during the build process
 
-    const response = await fetch('http://localhost:3000/api/meetups'); //api/meetups.js file
-    const data = await response.json();
+    const client = await MongoClient.connect(
+        "mongodb+srv://kalilginelysacko:a26CRQd24qz00khz@cluster0.ok1ioey.mongodb.net/meetups?retryWrites=true&w=majority"
+    );
+
+    const db = client.db();
+
+    /** access to collection */
+    const meetupsCollection =  db.collection('meetups');
+
+    /** insert into mongoDB */
+    const meetups = await meetupsCollection.find().toArray();
+
+    await client.close();
+
 
     return {
         props : {
-           meetups: data.meetups.map(meetup => ({
+           meetups: meetups.map(meetup => ({
                title: meetup.title,
                image: meetup.image,
                address: meetup.address,
